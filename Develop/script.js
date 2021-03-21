a0e2a4a595c8b7ebdda3ac534d8360f2497c7459
marlysco <marlysco94@gmail.com>
2021-03-21 14:00:44 -0400

    Adding script code

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,7 @@ var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"
 var lowerCaseArray= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numericArray=  ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCaractersArray = ["&","*", "%", "$", "@", "!"];
-var password=[];
+var passwordArray=[];
 
 // Prompting for options
 
@@ -18,13 +18,39 @@ while (passwordLength <= 8 || passwordLength >= 128) {
   var passwordLength= prompt("Enter the desired length, at least 8 characters and no more than 128")
 }
 
+var lowerCase=prompt("Do you want to have lower case letters");
 var upperCase=prompt("Do you want to have upper case letters");
 var numeric=prompt("Do you want to include numbers");
 var specialCaracters=prompt("Do you want to include special caracters");
 
-if  (upperCase=true) {
-  var password=
+//Function to collect in one array all the desired caracters, picked by the user
+
+function caractersCollect () {
+  if (lowerCase=true) {
+    var passwordArray = lowerCaseArray.concat(passwordArray);
+  }
+  console.log(passwordArray);
+  else if (upperCaseArray=true) {
+    var passwordArray = upperCaseArray.concat(passwordArray);
+  }
+  console.log(passwordArray);
+  else if (numeric=true) {
+    var passwordArray = numericArray.concat(passwordArray);
+  }
+  console.log(passwordArray);
+  else (specialCaractersArray=true) {
+    var passwordArray = specialCaractersArray.concat(passwordArray);
+  }
+  console.log(passwordArray);
 }
+
+caractersCollect ();
+
+
+
+
+
+
 
 
 
